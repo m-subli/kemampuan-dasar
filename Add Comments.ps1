c60aff29ae23b2463d2c6c948371d88fa319bb5c
Muhammad Subli <56763949+m-subli@users.noreply.github.com>
2019-10-29 11:00:15 +0700

    Add files via upload

---
+++ Add Comments.ps1
@@ -0,0 +1,9 @@
+﻿write-host " ==== PRAXIS ACADEMY ====
+   = Enterprise Full Stack Developer
+   = Enterprise Python Developer
+   = Enterprise Fron End Developer
+   = Node.js Programming "
+
+    Get-WmiObject -Class Win32_OperatingSystem –ComputerName localhost |
+        Select-Object -Property CSName,@{n=”Terakhir Boot”;
+    e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
---
+++ sending-email.ps1
@@ -0,0 +1,11 @@
+﻿$From = "annonym@gmail.com"
+$To = "15523094@students.uii.ac.id"
+$Cc = "subli.lee@gmail.com"
+$Attachment = "C:\temp\Some random file.txt"
+$Subject = "Email Subject"
+$Body = "Insert body text here"
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = "587"
+Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject `
+-Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl `
+-Credential (Get-Credential) -Attachments $Attachment
